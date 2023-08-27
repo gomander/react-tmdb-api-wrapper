a56c9f41b3a3922be022c19ff9e703bed0955dd6
Gunnar Omander <gunnar.omander@gmail.com>
2023-08-27 11:29:51 +0200

    Simplify env variable names

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,7 @@
-export const API_ROOT = import.meta.env.VITE_APP_API_ROOT
-export const IMAGE_ROOT = import.meta.env.VITE_APP_IMAGE_ROOT
-export const API_KEY = import.meta.env.VITE_APP_API_KEY
-export const ACCESS_TOKEN = import.meta.env.VITE_APP_ACCESS_TOKEN
+export const API_ROOT = import.meta.env.VITE_API_ROOT
+export const IMAGE_ROOT = import.meta.env.VITE_IMAGE_ROOT
+export const API_KEY = import.meta.env.VITE_API_KEY
+export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN
 
 if (!(API_ROOT && IMAGE_ROOT && API_KEY && ACCESS_TOKEN)) {
   throw new Error('Environment variables incorrectly configured!')
